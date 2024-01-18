af6100b9f0e6eb7016376befe04958efe3cbbb0f
GamerCleanVic <jonassd2000@gmail.com>
2024-01-18 16:43:43 -0500

    all prototype in 1 archive

---
+++ pkg_produto.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "teste/pkg_sqr_proto.h"
+
+int main(){
+    std::cout <<"A multiplicação = "<<produto(3,5)<< '\n';
+}
---
+++ pkg_sqr_rtn.cpp
@@ -0,0 +1,3 @@
+int produto(int x, int y){
+    return x * y;
+}
